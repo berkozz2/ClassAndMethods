061e68887a339247376b1bf91135a2c653d1ea38
ozmen <ozmen@DESKTOP-OTFBAI3>
2021-12-01 18:35:00 +0300

    Add project files.

---
+++ Methods/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Methods;
+
+Product product1 = new Product();
+product1.Id = 1;
+product1.ProductName = "Elma";
+product1.Price = 15;
+product1.ProductDescription = "Amasya Elması";
+
+Product product2 = new Product();
+product2.Id = 2;
+product2.ProductName = "Karpuz";
+product2.Price = 16;
+product2.ProductDescription = "Diyarbakır Karpuzu";
+
+Product[] products = new Product[] {product1, product2 };
+
+foreach (var product in products)
+{
+    Console.WriteLine(product.ProductName + " " +  "Kilosu"  + " " +   product.Price + "TL");
+    Console.WriteLine(product.ProductDescription);
+    Console.WriteLine("-----------------------");
+}
+
+Console.WriteLine("-------------Methods---------------");
+
+//Method bana tekrar tekrar kullanma imkanı verdi.
+
+SepetManager sepetManager = new SepetManager();
+sepetManager.Add();
+sepetManager.Add();
+sepetManager.Add();
+sepetManager.Add();
+
+
+
+//Console.WriteLine(description);
---
+++ Methods/SepetManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    //Bir class'ın sonunda Manager görürseniz bunlar bir operasyon tutuyor demektir.
+    //Eğer bir yerde normal bir parantez görürseniz anlayın ki orada bir metod çalışıyor.
+    public class SepetManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Tebrikler. Sepete Eklendi.");
+        }
+    }
+}
